9ae60f6c7f6d7323ae352a3d0048ca2ed2f9bde2
shreya2345-gn <dhanyya3334@gmail.com>
2025-04-08 02:21:56 -0400

    saved

---
+++ Add.java
@@ -0,0 +1,13 @@
+package Java277;
+
+public class Add {
+    public static void main(String[] args) {
+        // Define variables
+        int a = 5;
+        int b = 6;
+        int sum = a + b;
+
+        // Display the result
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+    }
+}
